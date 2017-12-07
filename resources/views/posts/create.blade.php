49a81d37e6b745233aca8b8f918470dbd1385046
Raphael Karanja <rkaranja@Cytonns-MacBook-Pro-2.local>
2017-12-07 09:37:06 +0300

    Adding create post form

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,4 +1,17 @@
-@extends(layouts.main)
+@extends('layouts.main')
 @section('content')
-  <h1>Creating a Post</h1>
+  <h4>Creating a Post</h4>
+  <hr/>
+  <form>
+    <div class="form-group">
+      <label for="exampleInputEmail1">Enter Post Title</label>
+      <input type="text" class="form-control" name="title"  aria-describedby="emailHelp" placeholder="Enter post Title">
+    </div>
+    <div class="form-group">
+      <label>Post Content</label>
+      <textarea class="form-control" name="postcontent" rows="8"></textarea>
+    </div>
+
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
 @endsection
